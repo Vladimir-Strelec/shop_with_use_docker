fdc69fb340c35b25f0a2fc71bc24d972315940e4
Vladimir-Strelec <vovikgithub@gmail.com>
2023-04-29 17:29:06 +0300

    Restrart project

--- shop_docker/shop_docker/account/custom_validators.py
+++
@@ -1,6 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def check_only_characters_in_name(value):
-    if value[0].isdigit():
-        raise ValidationError('Name cannot start with numbers')
